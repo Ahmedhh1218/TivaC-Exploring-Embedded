95280c9f715cda2b80744baadf225c639c4c5d68
Ahmed Hassabou <ahmed_hhassabou@outlook.com>
2023-08-05 00:04:48 +0300

    Update main.c

--- Session 3/Task 3/main.c
+++ Session 3/Task 3/main.c
@@ -4,8 +4,8 @@
 *
 *	Author: Ahmed Hisham Fathy Hassabou
 *
-*	Description: 	Main application source file. This file lights up the white led when PB1 or/and PB2 of the TivaC launchpad is pressed
-*								and turns the led off when the 2 buttons are released
+*	Description: 	Main application source file. This file uses the I2C communication method to communicate with an arduino UNO board and
+*			control its BUILTIN LED
 *
 *************************************************************************************************************************************************/
 
